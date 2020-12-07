f55b77d5aab2d13b579b2167ee7b4027f35bdf8e
Benjamin Blanchard <56205597+BenjaminB31@users.noreply.github.com>
2020-12-07 19:56:39 +0100

    Création des fichiers

---
+++ creation-user-ad.ps1
@@ -0,0 +1,17 @@
+﻿$users = Import-Csv -Delimiter ";" -Path "C:\Users\Administrateur.WIN-VIGS49SSAVO\Desktop\liste_eleves.csv"
+
+foreach ($user in $users){
+$pass= "Azerty!"
+$name = $user.nom + " " + $user.prenom
+$ou= "OU=" + $user.fonction + ",DC=GSB,DC=local"
+$nom= $user.nom
+$prenom= $user.prenom
+$pseudo= $user.prenom + "." + $user.nom
+try {
+ New-ADUser -Name $name  -givenname $pseudo -Path $ou
+            echo "Utilisateur ajouté : $name"    
+        } 
+catch{
+            echo "utilisateur non ajouté : $name"
+       }   
+   }
